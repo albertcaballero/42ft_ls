024fcece7e1b39b33e5cea80d5fcff868cfa88d8
albert <albertcaballerocoll@gmail.com>
2025-04-01 13:37:20 +0200

    basic flag checking

---
+++ ft_ls.h
@@ -0,0 +1,45 @@
+#ifndef FT_LS_H_
+#define FT_LS_H_
+
+#include<stdio.h>
+#include<sys/stat.h>
+
+
+
+//Define flags
+#define F_LONG		(1 << 0)
+#define F_RECURS	(1 << 1)
+#define F_ALL		(1 << 2)
+#define F_REVERSE	(1 << 3)
+#define F_TIME		(1 << 4) 
+
+/* Set a flag	user_flags	|= FLAG;
+Clear a flag	user_flags &= ~FLAG;
+Toggle a flag	user_flags ^= FLAG;
+Check a flag	if (user_flags & FLAG)
+*/
+
+enum Type {
+	FIL,
+	DIR,
+	SYML,
+	BLOCK,
+	DEV,
+	PIPE,
+	SOCK
+};
+
+typedef struct file
+{
+	char 	*name;
+	struct file	*next;
+	struct file	*subdir;
+	int		type;
+} t_file;
+
+struct ft_ls
+{
+	int flags;
+};
+
+#endif
